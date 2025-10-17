e61a23cf7cc6f5cddaa3c89148d54035dc8163a7
seven_bear <26707756+yueneiqi@users.noreply.github.com>
2025-10-17 11:51:16 +0800

    refactor: flatten command match arms

--- src/main.rs
+++ src/main.rs
@@ -14,36 +14,34 @@ async fn main() {
 
     // If no command is provided, show help
     let Some(command) = cli.command else {
-        Cli::parse_from(&["arc-cli-rs", "--help"]);
+        Cli::parse_from(["arc-cli-rs", "--help"]);
         return;
     };
 
     let result = match command {
-        Commands::ArcVersion => {
-            match arc::get_version().await {
-                Ok(version) => {
-                    println!("Arc Browser version: {}", version);
-                    Ok(())
-                }
-                Err(e) => Err(e),
+        Commands::ArcVersion => match arc::get_version().await {
+            Ok(version) => {
+                println!("Arc Browser version: {}", version);
+                Ok(())
             }
-        }
-        Commands::ListSpaces => {
-            match arc::get_spaces().await {
-                Ok(spaces) => {
-                    println!("Arc spaces:\n");
-                    for space in spaces {
-                        println!("{}: {}", space.id, space.title);
-                    }
-                    Ok(())
+            Err(e) => Err(e),
+        },
+        Commands::ListSpaces => match arc::get_spaces().await {
+            Ok(spaces) => {
+                println!("Arc spaces:\n");
+                for space in spaces {
+                    println!("{}: {}", space.id, space.title);
                 }
-                Err(e) => Err(e),
+                Ok(())
             }
-        }
-        Commands::SelectSpace { space_id } => {
-            arc::select_space(space_id).await
-        }
-        Commands::SelectSpaceName { space_name, case_insensitive, partial } => {
+            Err(e) => Err(e),
+        },
+        Commands::SelectSpace { space_id } => arc::select_space(space_id).await,
+        Commands::SelectSpaceName {
+            space_name,
+            case_insensitive,
+            partial,
+        } => {
             match arc::get_spaces().await {
                 Ok(spaces) => {
                     let space = spaces.iter().find(|s| {
@@ -69,29 +67,29 @@ async fn main() {
                 Err(e) => Err(e),
             }
         }
-        Commands::ListTabs => {
-            match arc::get_tabs().await {
-                Ok(tabs) => {
-                    println!("Arc tabs:\n");
-                    for tab in tabs {
-                        println!(
-                            "{}, {}, {}, {}, {:?}",
-                            tab.window_id, tab.tab_id, tab.title, tab.url, tab.location
-                        );
-                    }
-                    Ok(())
+        Commands::ListTabs => match arc::get_tabs().await {
+            Ok(tabs) => {
+                println!("Arc tabs:\n");
+                for tab in tabs {
+                    println!(
+                        "{}, {}, {}, {}, {:?}",
+                        tab.window_id, tab.tab_id, tab.title, tab.url, tab.location
+                    );
                 }
-                Err(e) => Err(e),
+                Ok(())
             }
-        }
+            Err(e) => Err(e),
+        },
         Commands::NewTab { url } => {
             let sanitized_url = sanitize_url(&url);
             arc::new_tab(&sanitized_url).await
         }
-        Commands::SelectTab { window_id, tab_id } => {
-            arc::select_tab(window_id, tab_id).await
-        }
-        Commands::SelectTabName { tab_name, case_insensitive, partial } => {
+        Commands::SelectTab { window_id, tab_id } => arc::select_tab(window_id, tab_id).await,
+        Commands::SelectTabName {
+            tab_name,
+            case_insensitive,
+            partial,
+        } => {
             match arc::get_tabs().await {
                 Ok(tabs) => {
                     let tab = tabs.iter().find(|t| {
@@ -117,12 +115,8 @@ async fn main() {
                 Err(e) => Err(e),
             }
         }
-        Commands::ReloadTab { window_id, tab_id } => {
-            arc::reload_tab(window_id, tab_id).await
-        }
-        Commands::CloseTab { window_id, tab_id } => {
-            arc::close_tab(window_id, tab_id).await
-        }
+        Commands::ReloadTab { window_id, tab_id } => arc::reload_tab(window_id, tab_id).await,
+        Commands::CloseTab { window_id, tab_id } => arc::close_tab(window_id, tab_id).await,
         Commands::NewLittleArc { url } => {
             let sanitized_url = sanitize_url(&url);
             arc::new_little_arc(&sanitized_url).await
